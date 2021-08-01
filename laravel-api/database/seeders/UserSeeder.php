7456159a75538612afa2b9f290acceec2ab4d550
SusmoySenGupta <susmoy.cse@gmail.com>
2021-08-02 00:29:54 +0600

    first commit

---
+++ laravel-api/database/seeders/UserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name'     => 'admin',
+            'email'    => 'admin@example.com',
+            'password' => bcrypt('secret'),
+            'role'     => 'admin',
+        ]);
+
+        User::create([
+            'name'     => 'user',
+            'email'    => 'user@example.com',
+            'password' => bcrypt('secret'),
+            'role'     => 'user',
+        ]);
+        
+        User::create([
+            'name'     => 'Another user',
+            'email'    => 'anotheruser@example.com',
+            'password' => bcrypt('secret'),
+            'role'     => 'another_user',
+        ]);
+    }
+}
---
+++ vue-client/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="w-screen h-screen flex justify-center items-center bg-gray-100 dark:bg-gray-900">
+        <div class="flex flex-col justify-center">
+            <div class="flex gap-10 justify-center items-center">
+                <img alt="Laravel logo" src="./assets/images/laravel-logo.png" class="w-20 h-20" />
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 dark:text-gray-500" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clip-rule="evenodd" />
+                </svg>
+                <img alt="Vue logo" src="./assets/images/logo.png" class="w-20 h-20" />
+            </div>
+            <h1 class="mt-8 text-2xl text-gray-800 dark:text-white">
+                Laravel-Vue Passport Authentication Starter
+            </h1>
+        </div>
+    </div>
+</template>
